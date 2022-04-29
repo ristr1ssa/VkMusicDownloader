d8b39c738193cdf218a2dae07e86faa6d15a8b1f
ristr1ssa <roba2005avagyan@gmail.com>
2022-04-29 13:37:12 +0300

    fiksh

--- vkAudio_Link/api.py
+++ vkAudio_Link/api.py
@@ -1,6 +1,4 @@
-from pydoc import resolve
 import aiohttp
-import asyncio
 import io
 
 class VkAudio:
@@ -24,10 +22,8 @@ class VkAudio:
                 resp_id = await resp_id.json()
                 resp_id = resp_id['response']['items']
                 ans = [f"{resp_id[elem]['owner_id']}_{resp_id[elem]['id']}={resp_id[elem]['title']}" for elem in range(len(resp_id))]
-                
-                await session.close()
-                await asyncio.sleep(0.1)
-                return ans
+
+            return ans
     
     async def get_urlById(self, ids_list: list[str]) -> list[str]:
         """
@@ -41,9 +37,6 @@ class VkAudio:
                 async with session.get(f'https://api.vk.com/method/audio.getById?audios={id}&access_token={self.token}&v=5.130') as resp_link:
                     resp_link = await resp_link.json()
                     ans.append(resp_link['response'][0]['url'])
-            
-            await session.close()
-            await asyncio.sleep(0.1)
             return ans
         
     async def byte_download(self, link: str) -> str:
@@ -56,8 +49,7 @@ class VkAudio:
             async with session.get(link) as resp:
                 ans = await resp.content.read()
                 ans = io.BytesIO(ans)
-                await session.close()
-                return ans
+            return ans
 
     async def get_title(self, id: int) -> list[str]:
         """
@@ -70,6 +62,4 @@ class VkAudio:
                 title = await title.json()
                 title = title['response'][0]['title']
 
-        await session.close()
-        await asyncio.sleep(0.1)
         return title
